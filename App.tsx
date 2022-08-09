0955fe41410848cc2fdf99e92fa12f0e9fc58c40
MarySamasat <mariana.cuadrado@samasatsa.com>
2022-08-09 16:02:21 -0500

    first commit

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export const App = () => {
+  return (
+    <View style={{flex:1, backgroundColor:'pink', justifyContent:'center', alignItems:'center'}}>
+      <Text style={{fontSize:25, color:'black'}}>Hola sr fish</Text>
+    </View>
+  )
+}
